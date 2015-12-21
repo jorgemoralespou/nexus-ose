b4f139f01cee0ab9b3d5b1bc93e39b27b8c54ee3
jorgemoralespou <jorgemoralespou@gmail.com>
2015-12-21 18:55:48 +0100

    Fixed jq not in image bug

--- nexus/nexus-container/scripts/addrepo.sh
+++ nexus/nexus-container/scripts/addrepo.sh
@@ -9,7 +9,7 @@ TEMPLATE_FILE="/tmp/repo.json"
 
 : ${NEXUS_USER:="admin"}
 : ${NEXUS_PASSWORD:="admin123"}
-: ${NEXUS_BASE_URL:="http://localhost:8081"}
+: ${NEXUS_BASE_URL:="http://localhost:8081"} # Avoid trailing slash
 
 read -d '' TEMPLATE << EOF
 {
@@ -53,7 +53,7 @@ repoURL=$2
 # TODO:
 
 function sedeasy {
-  echo "sed -i \"s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g\" $3"
+  echo "[DEBUG] sed -i \"s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g\" $3"
   sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
 }
 
@@ -74,29 +74,43 @@ function loadRepo {
    sedeasy "%ID%" "$_id" $TEMPLATE_FILE
    sedeasy "%REPO%" "$_url" $TEMPLATE_FILE
 
-   echo "Sending the following template for creating repo $_id"
+   echo "[INFO] Sending the following template for creating repo $_id"
    cat $TEMPLATE_FILE
 
    # Create repository configuration
-   curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -v -d "@$TEMPLATE_FILE" -u "${NEXUS_USER}:${NEXUS_PASSWORD}" "${NEXUS_BASE_URL}/service/local/repositories"
+   echo "curl -H 'Accept: application/json' -H 'Content-Type: application/json' -f -X POST -v -d \"@$TEMPLATE_FILE\" -u \"${NEXUS_USER}:${NEXUS_PASSWORD}\" \"${NEXUS_BASE_URL}/service/local/repositories\""
+   curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST -v -d "@$TEMPLATE_FILE" -u "${NEXUS_USER}:${NEXUS_PASSWORD}" "${NEXUS_BASE_URL}/service/local/repositories"
 
    # TODO: Instead of failing, check if the repository exist.
    _exit=$?
-   [ $_exit -ne 0 ] && echo "Error creating the hosted repository for $_id" && exit $_exit 
+   [ $_exit -ne 0 ] && echo "[WARN] Error creating the hosted repository for $_id" && exit $_exit 
+   echo ""
+   echo "[INFO] Repository created"
 
    # Adding repository configuration to public group.
    # We first query for current config and write it into a /tmp/group.json file
+   echo "[INFO] Getting information about public group"
    curl -s -H "Accept: application/json" -H "Content-Type: application/json" -f -X GET -u "${NEXUS_USER}:${NEXUS_PASSWORD}" -o /tmp/group.json "${NEXUS_BASE_URL}/service/local/repo_groups/public"
-   [ $_exit -ne 0 ] && echo "Error getting public group repository information" && exit $_exit 
+   [ $_exit -ne 0 ] && echo "[WARN] Error getting public group repository information" && exit $_exit 
+   echo ""
+   echo "[INFO] Public repo information retrieved"
+
 
    # then we add repository id
-   /tmp/jq '.' /tmp/group.json > /tmp/group-pretty.json
+   echo "[INFO] Add information about this repository to the public group" 
+   /usr/local/bin/jq '.' /tmp/group.json > /tmp/group-pretty.json
    sed -i -e "`wc -l /tmp/group-pretty.json | awk '{s=$1-3} END {print s}'` a\,{\"id\":\"$_id\"}" /tmp/group-pretty.json
+   echo ""
+   echo "[INFO] Information about this repository added to the public group" 
 
-   [ ! -f /tmp/group-pretty.json ] && echo "Something failed while trying to add the repository to the public group. Do it manually" && exit 2
+   [ ! -f /tmp/group-pretty.json ] && echo "[WARN] Something failed while trying to add the repository to the public group. Do it manually" && exit 2
    # and we load the new configuration for public group
+
+   echo "[INFO] Updating the public repo"
    curl -H "Accept: application/json" -H "Content-Type: application/json" -f -X PUT  -v -d "@/tmp/group-pretty.json" -u "${NEXUS_USER}:${NEXUS_PASSWORD}" "${NEXUS_BASE_URL}/service/local/repo_groups/public"
-   [ $_exit -ne 0 ] && echo "Error adding the hosted repository for $_id to the public group" && exit $_exit 
+   [ $_exit -ne 0 ] && echo "[WARN] Error adding the hosted repository for $_id to the public group" && exit $_exit 
+   echo ""
+   echo "[INFO] Public group updated"
 
    echo "------"
    echo "------"
