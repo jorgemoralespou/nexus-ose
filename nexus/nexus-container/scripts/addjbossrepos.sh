0244f50c67267ff67f9e22674628bc548c1287f1
Jorge Morales Pou <jorgemoralespou@users.noreply.github.com>
2016-10-14 18:29:42 +0200

    Added Redhat repos

--- nexus/nexus-container/scripts/addjbossrepos.sh
+++ nexus/nexus-container/scripts/addjbossrepos.sh
@@ -2,5 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-${DIR}/addrepo.sh jboss http://maven.repository.redhat.com/techpreview/all/
-${DIR}/addrepo.sh jboss-ce https://repository.jboss.org/nexus/content/groups/public/
+${DIR}/addrepo.sh jboss-ga       https://maven.repository.redhat.com/ga/
+${DIR}/addrepo.sh jboss-ea       https://maven.repository.redhat.com/earlyaccess/all/
+${DIR}/addrepo.sh jboss-ce       https://repository.jboss.org/nexus/content/groups/public/
+${DIR}/addrepo.sh jboss-techprev https://maven.repository.redhat.com/techpreview/all
